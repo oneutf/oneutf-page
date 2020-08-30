c4acaee2ddc5709533709bfc29e63cdd9ef06ca9
oneutf <oneutf>
2020-08-30 21:46:31 +0800

    1

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,46 +1,17 @@
 import axios from "axios";
 import QS from "qs";
 
-// axios.defaults.baseURL = "/api";
-axios.defaults.timeout = 10000;
-// axios.defaults.headers.post["Content-Type"] =
-//   "application/x-www-form-urlencoded";
+axios.defaults.headers["Content-type"] = "application/json;charset=utf-8";
 
-/**
- * get方法，对应get请求
- * @param {String} url [请求的url地址]
- * @param {Object} params [请求时携带的参数]
- */
-function get(url, params) {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(url, {
-        params
-      })
-      .then(res => {
-        resolve(res.data);
-      })
-      .catch(err => {
-        reject(err.data);
-      });
-  });
-}
+// 创建axios实例
+const serivce = axios.create({
+  timeout: 10000
+});
 
-/**
- *
- * @param {String} url
- * @param {Object} params
- */
-function post(url, params) {
-  return new Promise((resolve, reject) => {
-    axios
-      .post(url, QS.stringify(params))
-      .then(res => {
-        resolve(res.data);
-      })
-      .catch(err => {
-        reject(err.data);
-      });
-  });
-}
-export default { get, post };
+// 响应拦截器
+serivce.interceptors.response.use(res => {
+    
+})
+
+
+export default serivce;
